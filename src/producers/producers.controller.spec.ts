f724b03c684b150d1fa6ca2c6252603072fe52f1
Daniel Sansom <daniel.jon.sansom@gmail.com>
2022-08-02 20:33:52 +0200

    CI Implementation (#1)
    * Added grunt file and dockerignore.
    
    * Added grunt dependency.
    
    * Added grunt file.
    
    * Added dockerfile.
    
    * Updated docker file.
    
    * Fixed tests.
    
    * Updated docker file and added dev docker file
    
    * Moved and updated docker-compose file.
    
    * Added build file.
    
    * Updated build to use yarn.
    
    * Updated build node version.
    
    * Updated build file.
    
    * Updated build file.
    
    * Updated docker build file.
    
    * Updated build file.
    
    * Updated build file.
    
    * Remove .npmrc reference.
    
    * Updated Dockerfile.
    
    * Updated docker location.
    
    * Add version.
    
    * Fixed syntax error.
    
    * Added versioning.
    
    * ci: version bump to 0.0.3
    
    * Updated tag.
    
    * ci: version bump to 0.0.4
    
    * Updated tag.
    
    * ci: version bump to 0.0.5
    
    * Updated tag.
    
    * ci: version bump to 0.0.6
    
    * Updated tag.
    
    * Fixed syntax error.
    
    * ci: version bump to 0.0.7
    
    * Updated version.
    
    * ci: version bump to 0.0.8
    
    * Updated build.
    
    * Updated build.
    
    * Updated version.
    
    * ci: version bump to 0.0.9
    
    * Updated build.
    
    * ci: version bump to 0.0.10
    
    * Updated build.
    
    * ci: version bump to 0.0.11
    
    * Set port as environment variable.
    
    * ci: version bump to 0.0.12
    
    * Changed push condition on build.
    
    * ci: version bump to 0.0.13
    
    Co-authored-by: Automated Version Bump <gh-action-bump-version@users.noreply.github.com>

--- src/producers/producers.controller.spec.ts
+++ src/producers/producers.controller.spec.ts
@@ -1,18 +1,48 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProducersController } from './producers.controller';
+import { ProducersService } from './producers.service';
+
+const mockProducersService = () => ({
+  publish: jest.fn(),
+  publishBatch: jest.fn(),
+});
 
 describe('ProducersController', () => {
   let controller: ProducersController;
+  let producersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ProducersController],
+      providers: [
+        {
+          provide: ProducersService,
+          useFactory: mockProducersService,
+        },
+      ],
     }).compile();
 
     controller = module.get<ProducersController>(ProducersController);
+    producersService = module.get<ProducersService>(ProducersService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('publish', () => {
+    it('calls ProducersService.publish and returns the result.', async () => {
+      expect(producersService.publish).not.toHaveBeenCalled();
+
+      producersService.publish.mockResolvedValue('someValue');
+      const result = await producersService.publish({
+        topic: 'test-topic',
+        key: 'test-key',
+        value: 'test-value',
+      });
+
+      expect(producersService.publish).toHaveBeenCalled();
+      expect(result).toEqual('someValue');
+    });
   });
 });
--- src/producers/producers.service.spec.ts
+++ src/producers/producers.service.spec.ts
@@ -1,18 +1,34 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { KafkaIntegrationService } from '../kafka-integration/kafka-integration-service';
 import { ProducersService } from './producers.service';
+
+const mockKafkaIntegrationService = () => ({
+  createProducer: jest.fn(),
+});
 
 describe('ProducersService', () => {
   let service: ProducersService;
+  let kafkaIntegrationService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ProducersService],
+      providers: [
+        ProducersService,
+        {
+          provide: KafkaIntegrationService,
+          useFactory: mockKafkaIntegrationService,
+        },
+      ],
     }).compile();
 
+    kafkaIntegrationService = module.get<KafkaIntegrationService>(
+      KafkaIntegrationService,
+    );
     service = module.get<ProducersService>(ProducersService);
   });
 
   it('should be defined', () => {
+    console.log(service);
     expect(service).toBeDefined();
   });
 });
